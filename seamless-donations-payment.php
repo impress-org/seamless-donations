a4cef9da8a6ae61eeba8ac71d4ee596705eeec3c
David <david@Wally.local>
2015-08-10 19:34:22 -0400

    Add new seamless-donations-payment.php file.

---
+++ seamless-donations-payment.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ Seamless Donations by David Gewirtz, adopted from Allen Snook
+
+ Lab Notes: http://zatzlabs.com/lab-notes/
+ Plugin Page: http://zatzlabs.com/seamless-donations/
+ Contact: http://zatzlabs.com/contact-us/
+
+ Copyright (c) 2015 by David Gewirtz
+ */
+
